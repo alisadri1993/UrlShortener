3d1ef78d7db8950142c1134bcc9db5d67eef4134
sadri <alisadri1993@gmail.com>
2023-11-22 10:37:49 +0330

    create repository pattern with add and get methods

---
+++ Shortener.Endpoint/Infrastructure/Repositories/IShortLinkRepository.cs
@@ -0,0 +1,11 @@
+﻿using Shortener.Endpoint.Entities;
+
+namespace Shortener.Endpoint.Infrastructure.Repositories
+{
+    public interface IShortLinkRepository
+    {
+        Task<int> AddShortLinkAsync(ShortLink shortLink);
+        ShortLink? GetShortLinkByUrl(string url);
+        ShortLink? GetShortLinkByToken(string token);
+    }
+}
---
+++ Shortener.Endpoint/Infrastructure/Repositories/ShortLinkRepository.cs
@@ -0,0 +1,31 @@
+﻿using Shortener.Endpoint.Entities;
+using Shortener.Endpoint.Infrastructure.DataAccess;
+
+namespace Shortener.Endpoint.Infrastructure.Repositories
+{
+    public class ShortLinkRepository : IShortLinkRepository
+    {
+        private readonly UrlShortenerDbContext _dbContext;
+
+        public ShortLinkRepository(UrlShortenerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<int> AddShortLinkAsync(ShortLink shortLink)
+        {
+            await _dbContext.shortLinks.AddAsync(shortLink);
+            await _dbContext.SaveChangesAsync();
+            return shortLink.Id;
+        }
+
+        public ShortLink? GetShortLinkByToken(string token)
+        {
+            return _dbContext.shortLinks.FirstOrDefault(s => s.Token == token);
+        }
+
+        public ShortLink? GetShortLinkByUrl(string url)
+        {
+            return _dbContext.shortLinks.FirstOrDefault(s => s.Url == url);
+        }
+    }
+}
